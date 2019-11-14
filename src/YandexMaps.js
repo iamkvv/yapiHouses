dc66a60338e4a5d2bdfe62f8c1e59131787c8ddf
iamkvv <vkondra@gmail.com>
2019-11-14 13:31:25 +0500

    from work new 1

--- src/YandexMaps.js
+++ src/YandexMaps.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 import {Row,Col} from 'antd'
 import ReactDOMServer from 'react-dom/server';
+
+import ReactDOM from 'react-dom';
+
 import { YMaps, Map } from 'react-yandex-maps'
+
+import MyForm from './myform'
 
 //https://tech.yandex.ru/maps/jsbox/2.1/balloon_ajax
 
@@ -46,6 +51,110 @@ componentDidMount(){
             }
         })
     }
+
+
+    
+    setBalloonLayout = (prp) => {
+        const html = ReactDOMServer.renderToString(<MyForm test="qwerty" />);
+        console.log(html);
+        var Layout =  window.YAPI.templateLayoutFactory.createClass(
+            `<div id="balloon">${html}</div>`,
+            {
+                build: function () {
+                    Layout.superclass.build.call(this);
+                    ReactDOM.hydrate(
+                        <MyForm test= {prp} />,
+                        document.getElementById('balloon'),
+                    );
+                },
+            }
+        );
+        return Layout;
+    }
+
+    setSearch = (apiobj) => {
+
+        // Создаем экземпляр класса ymaps.control.SearchControl
+      var mySearchControl = new apiobj.control.SearchControl({
+            options: {
+                noPlacemark: true, //??
+                draggable: true,
+                provider: 'yandex#map',
+                noPopup:true,
+            }
+        });
+
+ 
+        // const html = ReactDOMServer.renderToString(<MyForm test="qwerty" />);
+        // console.log(html);
+        // var Layout = apiobj.templateLayoutFactory.createClass(
+        //     `<div id="balloon">${html}</div>`,
+        //     {
+        //         build: function () {
+        //             Layout.superclass.build.call(this);
+        //             ReactDOM.hydrate(
+        //                 <MyForm test="qwerty 123" />,
+        //                 document.getElementById('balloon'),
+        //             );
+        //         },
+        //     }
+        // );
+
+
+            // Результаты поиска будем помещать в коллекцию.
+         var  mySearchResults = new apiobj.GeoObjectCollection(null, {
+                hintContentLayout: apiobj.templateLayoutFactory.createClass('<h1>$[properties.name]</h1> <h2>{{properties.coord}}</h2>'),
+                //balloonContentLayout:Layout
+              //it's just test  balloonContentLayout:  apiobj.templateLayoutFactory.createClass('<div style="background-color:blue; padding:20px">123</div>')
+                // balloonContentLayout:  apiobj.templateLayoutFactory.createClass(
+                //     `<div id="balloon" style="width:auto">${html}</div>`,
+                // ),
+            });
+        
+console.log('mySearchResults',mySearchResults);
+mySearchResults["options"].set("balloonMinWidth", 700);
+mySearchResults["options"].set("balloonMaxWidth", 800);
+mySearchResults["options"].set("balloonMinHeight", 200);
+
+//mySearchResults["options"].set("zIndex", 9999); //??? не работает
+
+//mySearchResults["options"].set("autoPanDuration", 3000); //??
+
+//debugger;
+//https://yandex.ru/blog/mapsapi/47686
+
+            this.mapInstance.controls.add(mySearchControl);
+            this.mapInstance.geoObjects.add(mySearchResults);
+
+        // При клике по найденному объекту метка становится красной.
+        //mySearchResults.events.add('click', function (e) {
+        //    e.get('target').options.set('preset', 'islands#redIcon');
+        //});
+
+
+        // Выбранный результат помещаем в коллекцию.
+        var self = this;
+        mySearchControl.events.add('resultselect', function (e) {
+            var index = e.get('index');
+            //debugger; //mySearchControl.getResult(index)._value.geometry._coordinates
+
+            mySearchControl.getResult(index).then(function (res) {
+            let addr = res.properties.get('name'); //res.properties.get('text') res.properties.getAll()
+            res.properties.set("coord",res.geometry.getCoordinates()); //это мое св-во для hint'a
+
+              let ly = self.setBalloonLayout(addr);
+              res["options"].set("balloonContentLayout", ly  );
+
+              mySearchResults.add(res);
+            });
+        })
+            .add('submit', function () {
+                mySearchResults.removeAll();
+               // вроде удаляет ранее найденную метку
+        })
+        
+    }
+
 
     onLoadMap = (apiobj) => {
         window.YAPI = apiobj; //убрать window
@@ -130,9 +239,11 @@ componentDidMount(){
                                     center: this.state.centermap,// [55.157, 61.442], 
                                     type: 'yandex#map',
                                     zoom: this.props.zoom,
+                                   controls:[] //'searchControl'
                                 }}
                                 onLoad={(y) => {
-                                    this.onLoadMap(y)
+                                    this.onLoadMap(y);
+                                    this.setSearch(y);
                                 }}
                                 onClick={(e) => {
                                     let coord = e.get('coords');
@@ -146,7 +257,7 @@ componentDidMount(){
                                     })
 
                                     this.PMark.geometry._coordinates = coord;
-                                    e.originalEvent.map.geoObjects.removeAll();
+                                   ///tmp!!! e.originalEvent.map.geoObjects.removeAll(); вернуть
                                     e.originalEvent.map.geoObjects.add(this.PMark);
 
                                     this.getPanorama(coord);
